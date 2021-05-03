a28ef757e72fdc16d35fe79a3be81cdfdf102c11
jorgelucas <jorgelucasl91@gmail.com>
2021-05-03 18:30:07 -0300

    classe auxiliar para tratar as responses da api

---
+++ src/Estudos.App.WebApi/Services/Service.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Estudos.App.WebApi.Services
+{
+    public abstract class Service
+    {
+        protected StringContent ObterConteudo(object dado)
+        {
+            return new StringContent(
+                JsonSerializer.Serialize(dado),
+                Encoding.UTF8,
+                MediaTypeNames.Application.Json);
+        }
+
+
+        protected async Task<T> DeserializarObjetoResponse<T>(HttpResponseMessage responseMessage)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            return JsonSerializer.Deserialize<T>(await responseMessage.Content.ReadAsStringAsync(), options);
+        }
+
+        protected bool TratarErrosResponse(HttpResponseMessage response)
+        {
+
+            switch (response.StatusCode)
+            {
+                case HttpStatusCode.Unauthorized:
+                case HttpStatusCode.Forbidden:
+                case HttpStatusCode.NotFound:
+                case HttpStatusCode.InternalServerError:
+                    throw new HttpRequestException();// todo: criar uma exeption para tratar o erro
+
+                case HttpStatusCode.BadRequest:// possue msg de erros na resposta
+                    return false;
+            }
+
+            response.EnsureSuccessStatusCode();
+            return true;
+        }
+    }
+}

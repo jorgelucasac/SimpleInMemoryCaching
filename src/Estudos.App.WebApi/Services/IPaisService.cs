a5343dc71d6e2f39fa3bfe9d21becca3b73527aa
jorgelucas <jorgelucasl91@gmail.com>
2021-05-03 18:32:06 -0300

    add serviço de paises e metodo de consulta de todos os paises

---
+++ src/Estudos.App.WebApi/Services/IPaisService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Estudos.App.WebApi.ViewModels;
+
+namespace Estudos.App.WebApi.Services
+{
+    public interface IPaisService
+    {
+        Task<IEnumerable<PaisViewModel>> ObterTodos();
+    }
+}
---
+++ src/Estudos.App.WebApi/Services/PaisService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Estudos.App.WebApi.Extensions;
+using Estudos.App.WebApi.ViewModels;
+using Microsoft.Extensions.Options;
+
+namespace Estudos.App.WebApi.Services
+{
+    public class PaisService : Service, IPaisService
+    {
+        private readonly HttpClient _httpClient;
+
+        public PaisService(HttpClient httpClient, IOptions<AppSettings> appSettings)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(appSettings.Value.ApiPaisesUrl);
+        }
+
+        public async Task<IEnumerable<PaisViewModel>> ObterTodos()
+        {
+            var response = await _httpClient.GetAsync("/rest/v2/all");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<IEnumerable<PaisViewModel>>(response);
+        }
+    }
+}

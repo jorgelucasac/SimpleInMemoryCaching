b71dd83c99bb243473ec2665b4f89cd1e05f5f0e
jorgelucas <jorgelucasl91@gmail.com>
2021-05-03 18:33:11 -0300

    add controller de pais com listagem de todos os paises

---
+++ src/Estudos.App.WebApi/Controllers/PaisesController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Estudos.App.WebApi.Services;
+using Estudos.App.WebApi.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Estudos.App.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaisesController : Controller
+    {
+        private readonly IPaisService _paisService;
+
+        public PaisesController(IPaisService paisService)
+        {
+            _paisService = paisService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PaisViewModel>>> ObterTodos()
+        {
+            var paises = await _paisService.ObterTodos();
+            return Ok(paises);
+        }
+    }
+}

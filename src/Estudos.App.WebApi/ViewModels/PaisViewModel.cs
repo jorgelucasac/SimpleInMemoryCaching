c23e3b56c73e0e679f7d72716ca2823b188e05b5
jorgelucas <jorgelucasl91@gmail.com>
2021-05-03 17:35:09 -0300

    view model de pais

---
+++ src/Estudos.App.WebApi/ViewModels/PaisViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+namespace Estudos.App.WebApi.ViewModels
+{
+    public class Moeda
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+    }
+
+    public class Idioma
+    {
+        public string Iso6391 { get; set; }
+        public string Iso6392 { get; set; }
+        public string Name { get; set; }
+        public string NativeName { get; set; }
+    }
+
+    public class Traducao
+    {
+        public string De { get; set; }
+        public string Es { get; set; }
+        public string Fr { get; set; }
+        public string Ja { get; set; }
+        public string It { get; set; }
+        public string Br { get; set; }
+        public string Pt { get; set; }
+    }
+
+    public class BlocoRegional
+    {
+        public string Acronym { get; set; }
+        public string Name { get; set; }
+        public List<string> OtherAcronyms { get; set; }
+        public List<string> OtherNames { get; set; }
+    }
+
+    public class Pais
+    {
+        public string Name { get; set; }
+        public List<string> TopLevelDomain { get; set; }
+        public string Alpha2Code { get; set; }
+        public string Alpha3Code { get; set; }
+        public List<string> CallingCodes { get; set; }
+        public string Capital { get; set; }
+        public List<string> AltSpellings { get; set; }
+        public string Region { get; set; }
+        public string Subregion { get; set; }
+        public int Population { get; set; }
+        public List<double> Latlng { get; set; }
+        public string Demonym { get; set; }
+        public double Area { get; set; }
+        public double Gini { get; set; }
+        public List<string> Timezones { get; set; }
+        public List<string> Borders { get; set; }
+        public string NativeName { get; set; }
+        public string NumericCode { get; set; }
+        public List<Moeda> Currencies { get; set; }
+        public List<Idioma> Languages { get; set; }
+        public Traducao Translations { get; set; }
+        public string Flag { get; set; }
+        public List<BlocoRegional> RegionalBlocs { get; set; }
+        public string Cioc { get; set; }
+    }
+
+}

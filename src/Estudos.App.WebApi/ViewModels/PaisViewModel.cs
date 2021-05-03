1a3e6328919c678553e0bdc28eeb45fe8fcf8450
jorgelucas <jorgelucasl91@gmail.com>
2021-05-03 18:24:35 -0300

    corrigindo view model de paises para aceitar valores nulos em proprieades numericas

--- src/Estudos.App.WebApi/ViewModels/PaisViewModel.cs
+++ src/Estudos.App.WebApi/ViewModels/PaisViewModel.cs
@@ -36,7 +36,7 @@ namespace Estudos.App.WebApi.ViewModels
         public List<string> OtherNames { get; set; }
     }
 
-    public class Pais
+    public class PaisViewModel
     {
         public string Name { get; set; }
         public List<string> TopLevelDomain { get; set; }
@@ -47,11 +47,11 @@ namespace Estudos.App.WebApi.ViewModels
         public List<string> AltSpellings { get; set; }
         public string Region { get; set; }
         public string Subregion { get; set; }
-        public int Population { get; set; }
+        public int? Population { get; set; }
         public List<double> Latlng { get; set; }
         public string Demonym { get; set; }
-        public double Area { get; set; }
-        public double Gini { get; set; }
+        public double? Area { get; set; }
+        public double? Gini { get; set; }
         public List<string> Timezones { get; set; }
         public List<string> Borders { get; set; }
         public string NativeName { get; set; }

299eb92dc01108acdc00453fe54cfaef7652e91f
jorgelucas <jorgelucasl91@gmail.com>
2021-05-04 14:56:24 -0300

    implementação do serviço de cache de responses

---
+++ src/Estudos.App.WebApi/Services/Interfaces/IResponseCacheService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Estudos.App.WebApi.Services.Interfaces
+{
+    public interface IResponseCacheService
+    {
+        void SalvarCache(string key, object value, int tempoDuracaoCache);
+        object ObterCache(string key);
+
+    }
+}
---
+++ src/Estudos.App.WebApi/Services/ResponseCacheService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Estudos.App.WebApi.Services.Interfaces;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Estudos.App.WebApi.Services
+{
+    public class ResponseCacheService : IResponseCacheService
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public ResponseCacheService(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+
+        public void SalvarCache(string key, object value, int tempoDuracaoCache)
+        {
+            var memoryCacheEntryOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(tempoDuracaoCache),
+                SlidingExpiration = TimeSpan.FromSeconds((tempoDuracaoCache / 2.0))
+            };
+
+            _memoryCache.Set(key, value, memoryCacheEntryOptions);
+        }
+
+        public object ObterCache(string key)
+        {
+            return _memoryCache.TryGetValue(key, out var cacheValue) ? cacheValue : null;
+        }
+
+    }
+}
